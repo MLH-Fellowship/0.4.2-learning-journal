58e2ba4c84d3f59d653bdce1bfc0b3d6e3f6fb6e
Nilarjun Das <nilarjundas@outlook.com>
2020-06-03 20:00:50 +0530

    added home-screen and Journal Feed (#11)
    * added home-screen
    
    * journal feed screen added
    
    * minor refactoring

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -2,7 +2,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import * as React from 'react';
 
 import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
+import HomeScreen from '../screens/JournalFeed';
 import LinksScreen from '../screens/LinksScreen';
 
 const BottomTab = createBottomTabNavigator();
@@ -12,7 +12,7 @@ export default function BottomTabNavigator({ navigation, route }) {
   // Set the header title on the parent stack navigator depending on the
   // currently active tab. Learn more in the documentation:
   // https://reactnavigation.org/docs/en/screen-options-resolution.html
-  navigation.setOptions({ headerTitle: getHeaderTitle(route) });
+  navigation.setOptions({ headerShown: false });
 
   return (
     <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
