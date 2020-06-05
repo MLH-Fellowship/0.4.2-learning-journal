7453ccd66dbb1917340eac9f506939e817527012
Ansh Godha <ag759@cornell.edu>
2020-06-05 12:40:14 +0530

    heatmap integration

--- components/Buttons/LoggerButton.js
+++ components/Buttons/LoggerButton.js
@@ -1,24 +1,37 @@
-import React from "react";
-import { MaterialIcons } from "@expo/vector-icons";
-import { TouchableOpacity, StyleSheet } from "react-native";
+import React, { Fragment } from "react";
+import { MaterialIcons, Ionicons } from "@expo/vector-icons";
+import { TouchableOpacity, StyleSheet, View } from "react-native";
 import Color from "../../constants/Colors";
 
-export default function ({ style, size, callback }) {
+const BUTTON_DIMENSION = 60;
+
+export default function ({ style, size, callback, heatmapCallback }) {
   return (
-    <TouchableOpacity
-      style={[{ ...style }, styles.buttonStyle]}
-      onPress={callback}
-    >
-      <MaterialIcons name="add" size={size || 30} color="white" />
-    </TouchableOpacity>
+    <View>
+      <TouchableOpacity
+        style={[
+          { ...style, marginBottom: BUTTON_DIMENSION + 5 },
+          styles.buttonStyle,
+        ]}
+        onPress={heatmapCallback}
+      >
+        <Ionicons name="md-flame" size={size || 30} color="white" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[{ ...style }, styles.buttonStyle]}
+        onPress={callback}
+      >
+        <MaterialIcons name="add" size={size || 30} color="white" />
+      </TouchableOpacity>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   buttonStyle: {
-    width: 60,
-    height: 60,
-    borderRadius: 30,
+    width: BUTTON_DIMENSION,
+    height: BUTTON_DIMENSION,
+    borderRadius: BUTTON_DIMENSION / 2,
     backgroundColor: Color.darkPurple,
     alignItems: "center",
     justifyContent: "center",
--- screens/Journal.js
+++ screens/Journal.js
@@ -1,5 +1,5 @@
 import * as WebBrowser from "expo-web-browser";
-import * as React from "react";
+import React, { useState } from "react";
 import {
   Image,
   Platform,
@@ -7,15 +7,36 @@ import {
   Text,
   TouchableOpacity,
   View,
+  Modal,
 } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import LoggerButton from "../components/Buttons/LoggerButton";
-
+import Heatmap from "../components/Heatmap";
 import JournalList from "../components/Posts/JournalPostList";
 
+const commitsData = [
+  { date: "2020-06-02", count: 1 },
+  { date: "2020-06-03", count: 2 },
+  { date: "2020-06-04", count: 3 },
+  { date: "2020-06-05", count: 4 },
+  { date: "2020-06-06", count: 5 },
+  { date: "2020-05-30", count: 2 },
+  { date: "2020-05-31", count: 3 },
+  { date: "2020-05-01", count: 2 },
+  { date: "2020-05-02", count: 4 },
+  { date: "2020-05-05", count: 2 },
+  { date: "2020-05-30", count: 4 },
+];
+
 export default function JournalFeed({ navigation }) {
+  const [modalVisible, setModalVisible] = useState(false);
+
   function handleButtonPress() {
     navigation.navigate("loggerModal");
+  }
+
+  function handleHeatPress() {
+    setModalVisible(true);
   }
 
   return (
@@ -34,7 +55,29 @@ export default function JournalFeed({ navigation }) {
         </View>
       </View>
       <JournalList />
-      <LoggerButton style={styles.logButton} callback={handleButtonPress} />
+      <LoggerButton
+        style={styles.logButton}
+        callback={handleButtonPress}
+        heatmapCallback={handleHeatPress}
+      />
+      <Modal animationType="slide" transparent visible={modalVisible}>
+        <View style={styles.modalView}>
+          <TouchableOpacity onPress={() => setModalVisible(false)}>
+            <Text style={{ alignSelf: "flex-end" }}>Close</Text>
+          </TouchableOpacity>
+          <Heatmap
+            style={{
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "center",
+              marginTop: 40,
+            }}
+            width={200}
+            height={350}
+            dateDetails={commitsData}
+          />
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -43,7 +86,15 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#fff",
     flex: 1,
+    justifyContent: "center",
   },
+
+  centeredView: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+
   topContainer: {
     backgroundColor: "#F9ECFF",
     display: "flex",
@@ -84,5 +135,23 @@ const styles = StyleSheet.create({
     position: "absolute",
     bottom: 10,
     right: 10,
+    height: 60,
+  },
+
+  modalView: {
+    flex: 0.8,
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
 });
